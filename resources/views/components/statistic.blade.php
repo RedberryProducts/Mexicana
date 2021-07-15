a279543ae0956041ab3a799507b6ecae3cd9d802
giunashvili <giorgi.official@yahoo.com>
2021-07-15 21:58:51 +0400

    fix: card text centering

--- resources/views/components/statistic.blade.php
+++ resources/views/components/statistic.blade.php
@@ -1,6 +1,6 @@
-<div class="mx-10 group flex items-center bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5 bg-blue-900 bg-opacity-100 transition">
-	<div>
-		<span class="text-white">{{ $name }}</span>
-		<span class="text-xs text-blue-300 block">clicks: {{ $clicks }}</span>
+<div class="mx-10 group sm:w-1/5 flex items-center justify-center bg-opacity-40 shadow-xl gap-5 px-6 py-5 rounded-lg ring-2 ring-offset-2 ring-offset-blue-800 ring-cyan-700 mt-5 bg-blue-900 bg-opacity-100 transition">
+	<div class="flex flex-col justify-center">
+		<span class="text-white text-center">{{ $name }}</span>
+		<span class="text-xs text-blue-300 block text-center">clicks: {{ $clicks }}</span>
 	</div>
 </div>
