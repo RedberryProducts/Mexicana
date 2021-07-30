f77c51e87d8056253150c3508683cee41e1260b1
giunashvili <giorgi.official@yahoo.com>
2021-07-30 20:46:32 +0400

    add: fb pixels code

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -167,7 +167,21 @@
 	</div>
 	<!--END MOBILE SECTION HTML-->
 
-  <script defer src="{{ asset('assets/alpine.min.js') }}"></script>
-	<script src="{{ asset('assets/main.js') }}"></script>
+    <script defer src="{{ asset('assets/alpine.min.js') }}"></script>
+    <script src="{{ asset('assets/main.js') }}"></script>
+
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '570993467245659');
+        fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=570993467245659&ev=PageView&noscript=1" /></noscript>
 </body>
 </html>
